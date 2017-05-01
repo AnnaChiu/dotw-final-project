7a5285c7bf9af4e48800b72bf370aec40a3309b8
Anna Chiu <awc316@nyu.edu>
2017-05-01 19:20:33 -0400

    drawing 2 complete (rain)

--- javascript/rain.js
+++ javascript/rain.js
@@ -1,7 +1,164 @@
+var myCanvas;
+var clouds;
+var rain = [];
+var rainDirection = 0;
+
+var umbrella;
+var umbrellaMouse;
+
+function preload(){
+	clouds = loadImage("images/huge-clouds.png");
+	umbrella = loadImage("images/yellow-umbrella.png");
+}
+
 function setup() {
-  
+	myCanvas = createCanvas(windowWidth, windowHeight);
+
+	// set up 
+	ellipseMode(CENTER);
+	imageMode(CENTER);
+	noCursor();
+
+	// rain that starts in the clouds
+	for(var drops = 0; drops < 100; drops++){
+		rain.push(new Rain(random(0, windowWidth),-random(0,250)));
+	}
+
+	// rain that starts anywhere on the screen
+	for(var drops = 0; drops < 100; drops++){
+		rain.push(new Rain(random(0, windowWidth),random(0,windowHeight)));
+	}
+
+	umbrellaMouse = new Umbrella(windowWidth/2, windowHeight);
 }
 
 function draw() {
-  
+	background(0, 64, 128);
+
+	// make it rain
+	for(var drops = 0; drops < rain.length; drops++){
+		push();
+		rain[drops].rotate();
+		rain[drops].display();
+		rain[drops].move();
+		pop();
+
+		collisionUmbrellaToRain(umbrellaMouse, rain[drops]);
+	}
+
+	translate();
+
+	// have umbrella move with mouse
+	umbrellaMouse.display(mouseX, mouseY);
+
+  	image(clouds,windowWidth/2,windowHeight/6,windowWidth,windowHeight/3);
 }
+
+// code per raindrop
+function Rain(x,y){
+	this.x = x;
+	this.y = y;
+
+	// randomly determine the rate of rain for this rain drop
+	this.rainRate = random(2,5);
+
+	// display the rain drops
+	this.display = function(){
+		fill(128, 191, 255);
+		stroke(128, 191, 255);
+
+		ellipse(this.x, this.y, 5, 15);
+	}
+
+	// move the rain downwards at all times and have the rain wrap around to fall again
+	// when it reaches the bottom of the window + 200
+	this.move = function(){
+		// move rain down
+		if(this.y < windowHeight + 200){
+			this.y += this.rainRate;
+		}
+		else{
+			this.y = -random(100,200);
+			this.x = random(0, windowWidth);
+			this.rainRate = random(2,5);
+		}
+	}
+
+	// rotate rain in direction according to mouse click
+	this.rotate = function(){
+		// move rain in a direction
+		if(rainDirection == 0){
+			// this.x += 0;
+		}
+		else if(rainDirection == -1){
+			// this.x += -1;
+			rotate(PI/15);
+			
+		}
+		else if(rainDirection == 1){
+			// this.x += 1;
+			rotate(-PI/15);	
+		}
+	}
+}
+
+// umbrella as mouse
+function Umbrella(x,y) {
+	this.x = x;
+	this.y = y;
+
+	this.display = function(x,y){
+		this.x = x;
+		this.y = y;
+
+		image(umbrella,this.x,this.y,300,300);
+		// ellipse(this.x,this.y, 300, 300);
+	}
+}
+
+// check for collision between raindrop and umbrella
+function collisionUmbrellaToRain(umbrella, raindrop){
+	var distance = dist(umbrella.x, umbrella.y, raindrop.x, raindrop.y);
+
+	if(distance < 150){
+		console.log("collision!");
+		moveAway(umbrella, raindrop);
+	}
+}
+
+// if collision occured, then have the rain move away from the umbrella
+function moveAway(umbrella, raindrop){
+	var distance = dist(umbrella.x, umbrella.y, raindrop.x, raindrop.y);
+	if(distance < 160){
+		raindrop.y -= 1;
+		if(raindrop.x > umbrella.x){
+			raindrop.x += 1;
+		}
+		else{
+			raindrop.x -= 1;
+		}
+	}
+}
+
+// use mouse clicks to determine direction of rain
+function mouseClicked(){
+	// click right side of screen
+	if(mouseX > 2 * (windowWidth/3)){
+		console.log("right side clicked");
+		rainDirection = 1;
+	}
+	// click on the middle portion of the screen
+	else if(mouseX > windowWidth/3){
+		console.log("middle clicked");
+		rainDirection = 0;
+	}
+	// click on left side of screen
+	else{
+		console.log("left side clicked");
+		rainDirection = -1;
+	}
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+} 
